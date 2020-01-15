1a3106b5f4bfd5c58b38f07fd512c98bf4337b37
Stallix <joey@evolution-x.org>
2020-01-15 19:06:34 +0000

    FODCircleView: FOD pressed state [1/2]
    * Cyan vs Green war continues
    * Default option set to fod_icon_pressed due to previous FOD impl changes
    * White joins the war (MIUI V11.0.2 Global)
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Change-Id: I4c79b8fbf8ae50901f938775abbf3596e96659d0

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -196,7 +196,32 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         super.onDraw(canvas);
 
         if (mIsCircleShowing) {
-            canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+            if (getFODPressedState() == 0) {
+                //canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+                setImageResource(R.drawable.fod_icon_pressed);
+            } else if (getFODPressedState() == 1) {
+                //canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+                setImageResource(R.drawable.fod_icon_pressed_white);
+            } else if (getFODPressedState() == 2) {
+                canvas.drawCircle(mSize / 2, mSize / 2, mSize / 2.0f, mPaintFingerprint);
+            }
+        }
+    }
+
+    private int getFODPressedState() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.FOD_PRESSED_STATE, 0);
+    }
+
+    private void setFODPressedState() {
+        int fodpressed = getFODPressedState();
+
+        if (fodpressed == 0) {
+            setImageResource(R.drawable.fod_icon_pressed);
+        } else if (fodpressed == 1) {
+            setImageResource(R.drawable.fod_icon_pressed_white);
+        } else if (fodpressed == 2) {
+            setImageDrawable(null);
         }
     }
 
@@ -310,7 +335,7 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
         updateAlpha();
         dispatchPress();
 
-        setImageResource(R.drawable.fod_icon_pressed);
+        setFODPressedState();
         setColorFilter(Color.argb(0,0,0,0), PorterDuff.Mode.SRC_ATOP);
         invalidate();
     }
