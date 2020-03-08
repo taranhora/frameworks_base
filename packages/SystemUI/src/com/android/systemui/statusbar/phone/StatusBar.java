ab21965437fc9fe0ee2f82033b417897f373d951
SuperDroidBond <superdroidbond@yahoo.com>
2020-03-08 15:09:19 +0530

    KG Charging Animation: Add more animation for users [1/2]
    Special Thanks to @Rodolphe06, Flowdor Theme Dev for the json files
    
    Change-Id: I73da145a658cacd9e03840cf015da1d843731b4e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -518,7 +518,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     // LS visualizer on Ambient Display
     private boolean mAmbientVisualizer;
 
-    private boolean mChargingAnimation;
+    private int mChargingAnimation;
 
     private final BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -4585,9 +4585,9 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private void updateChargingAnimation() {
         mChargingAnimation = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_CHARGING_ANIMATION, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.LOCKSCREEN_CHARGING_ANIMATION, 1, UserHandle.USER_CURRENT);
         if (mKeyguardIndicationController != null) {
-            mKeyguardIndicationController.updateChargingIndication(mChargingAnimation);
+            mKeyguardIndicationController.updateChargingIndication();
         }
     }
 
